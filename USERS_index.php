97681d33fd69ae8083f33f01b77b7a154b62cbf4
manolis <manolis>
2006-10-07 09:23:47 +0000

    Scare:  Minor rewrite.  Functionality unchanged.

--- USERS_index.php
+++ USERS_index.php
@@ -1,27 +1,35 @@
-<?
+<?php
+
 /************************************************************************/
-/* Leonardo: Gliding XC Server					                        */
-/* ============================================                         */
-/*                                                                      */
-/* Copyright (c) 2004-5 by Andreadakis Manolis                          */
-/* http://leonardo.thenet.gr                                            */
-/* http://sourceforge.net/projects/leonardoserver                       */
-/*                                                                      */
+/* Leonardo: Gliding XC Server											*/
+/* ============================================							*/
+/*																		*/
+/* Copyright (c) 2004-5 by Andreadakis Manolis							*/
+/* http://leonardo.thenet.gr											*/
+/* http://sourceforge.net/projects/leonardoserver						*/
+/*																		*/
 /* This program is free software. You can redistribute it and/or modify */
 /* it under the terms of the GNU General Public License as published by */
-/* the Free Software Foundation; either version 2 of the License.       */
+/* the Free Software Foundation; either version 2 of the License.		*/
 /************************************************************************/
-	require_once dirname(__FILE__)."/users/users.css";
 
-	$opPage=$_GET['page'];
-	$userAction=$_GET['act'];
+require_once dirname(__FILE__).'/users/users.css';
 
-	if ($opPage=="admin") {
-		require_once dirname(__FILE__)."/users/admin.php";
-	} else 	if ($opPage=="index") {
-		require_once dirname(__FILE__)."/users/index.php";
-	} else 	if ($opPage=="sendmail") {
-		require_once dirname(__FILE__)."/users/sendmail.php";
-	}
+$userAction=$_GET['act'];
+
+switch ($opPage=$_GET['page'])
+{
+case 'admin':
+	require_once dirname(__FILE__).'/users/admin.php';
+	break;
+
+case 'index':
+	require_once dirname(__FILE__).'/users/index.php';
+	break;
+
+case 'sendmail':
+	require_once dirname(__FILE__).'/users/sendmail.php';
+	break;
+}
 
 ?>
