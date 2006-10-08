f3391e2910a7bfa665d19f96cdb0763f26350125
manolis <manolis>
2006-10-08 22:48:41 +0000

    *** empty log message ***

--- GUI_flight_delete.php
+++ GUI_flight_delete.php
@@ -1,6 +1,7 @@
-<?
+<?php
+
 /************************************************************************/
-/* Leonardo: Gliding XC Server					                                */
+/* Leonardo: Gliding XC Server											*/
 /* ============================================                         */
 /*                                                                      */
 /* Copyright (c) 2004-5 by Andreadakis Manolis                          */
@@ -11,25 +12,47 @@
 /* the Free Software Foundation; either version 2 of the License.       */
 /************************************************************************/
 
-  $flightID=$_REQUEST["flightID"];
-  $confirmed=$_REQUEST["confirmed"];
+$flightID	=$_REQUEST['flightID'];
+$confirmed	=$_REQUEST['confirmed'];
 
-  $flight=new flight();
-  $flight->getFlightFromDB($flightID);
+$flight=new flight();
+$flight->getFlightFromDB($flightID);
 
-  if ($confirmed && ( $flight->userID == $userID || in_array($userID,$admin_users) )  ) {
-
+if ($confirmed && ($flight->userID==$userID || in_array($userID,$admin_users)))
+{
 	$flight->deleteFlight();
-	echo "<div align=center><br>"._THE_FLIGHT_HAS_BEEN_DELETED."<br><br>";
-	echo "<a href='?name=".$module_name."&op=list_flights'>"._RETURN."</a><br></div>";
-  } else {
-	  $location=formatLocation(getWaypointName($flight->takeoffID),$flight->takeoffVinicity,$takeoffRadious);
-	
-	  open_inner_table(_CAUTION_THE_FLIGHT_WILL_BE_DELETED,600,"delete_icon.png");
-	  echo "<div align=center><br><b>"._PILOT.": ".$flight->userName." &nbsp;&nbsp; "._THE_DATE.": ".formatDate($flight->DATE)."  &nbsp;&nbsp; "._TAKEOFF_LOCATION.": ".$location."</b> ";
-	  echo "<br><br><a href='?name=".$module_name."&op=delete_flight&flightID=".$flightID."&confirmed=1'>"._YES."</a> | <a href='javascript:history.go(-1)'>"._NO."</a>";
-	  echo "<br></div>";
-	  close_inner_table();
-  }
-  
+
+	echo '<div align="center"><br/>'
+			._THE_FLIGHT_HAS_BEEN_DELETED.'<br/><br/>'
+			.'<a href="?name='.$module_name.'&op=list_flights">'._RETURN.'</a><br/>'
+		.'</div>';
+}
+else
+{
+	$location=formatLocation(getWaypointName($flight->takeoffID),$flight->takeoffVinicity,$takeoffRadious);
+
+	open_inner_table(_CAUTION_THE_FLIGHT_WILL_BE_DELETED,600,'delete_icon.png');
+
+	echo '<div align="center"><br/>'
+			.'<b>'
+				._PILOT.': '
+				.$flight->userName.' &nbsp;&nbsp; '
+				._THE_DATE.': '
+				.formatDate($flight->DATE)
+				.'  &nbsp;&nbsp; '
+				._TAKEOFF_LOCATION.': '
+				.$location
+			.'</b><br/><br/>'
+			.'<a href="?name='.$module_name.'&op=delete_flight&flightID='.$flightID.'&confirmed=1">'
+				._YES
+			.'</a>'
+			.' | '
+			.'<a href="javascript:history.go(-1)">'
+				._NO
+			.'</a><br/>'
+		.'</div>';
+
+	close_inner_table();
+}
+
 ?>
