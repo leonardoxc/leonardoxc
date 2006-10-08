ff8dea4d277b3fe064715c1694835079897589f9
manolis <manolis>
2006-10-08 22:54:07 +0000

    *** empty log message ***

--- GUI_sites.php
+++ GUI_sites.php
@@ -1,6 +1,7 @@
-<?  
+<?php
+
 /************************************************************************/
-/* Leonardo: Gliding XC Server					                                */
+/* Leonardo: Gliding XC Server											*/
 /* ============================================                         */
 /*                                                                      */
 /* Copyright (c) 2004-5 by Andreadakis Manolis                          */
@@ -11,36 +12,37 @@
 /* the Free Software Foundation; either version 2 of the License.       */
 /************************************************************************/
 
-	$pilotsList=array();
-	$pilotsID=array();
-	
-	list($takeoffs,$takeoffsID)=getTakeoffList();
-	list($countriesCodes,$countriesNames)=getCountriesList();
+$pilotsList	=array();
+$pilotsID	=array();
 
-	require $moduleRelPath."/CL_template.php";
-	$Ltemplate = new LTemplate($moduleRelPath.'/templates/'.$PREFS->themeName);
+list($takeoffs,$takeoffsID)=getTakeoffList();
+list($countriesCodes,$countriesNames)=getCountriesList();
 
-	$Ltemplate ->set_filenames(array(
-		'body' => 'sites.html')
-	);
+require $moduleRelPath.'/CL_template.php';
+$Ltemplate=new LTemplate($moduleRelPath.'/templates/'.$PREFS->themeName);
 
-	$COUNTRIES_OPTION_LIST="";
-	for($k=0;$k<count($countriesCodes);$k++) {
-		$sel=($countriesCodes[$k]==$FILTER_country1_select)?"selected":"";
-		$COUNTRIES_OPTION_LIST.="<option value='".$countriesCodes[$k]."' $sel>".$countriesNames[$k]." (".$countriesCodes[$k].")</option>\n";
-	}
+$Ltemplate->set_filenames(array(
+	'body'=>'sites.html')
+);
 
-	$TAKEOFF_OPTION_LIST="";
-	for($k=0;$k<count($takeoffs);$k++) {
-		$sel=($takeoffsID[$k]==$FILTER_takeoff1_select)?"selected":"";
-		 $TAKEOFF_OPTION_LIST.="<option value='".$takeoffsID[$k]."' $sel>".$takeoffs[$k]."</option>\n";
-	}
+for ($k=0, $COUNTRIES_OPTION_LIST=''; $k<count($countriesCodes); ++$k)
+{
+	$sel=($countriesCodes[$k]==$FILTER_country1_select) ? ' selected' : '';
+	$COUNTRIES_OPTION_LIST.='<option value="'.$countriesCodes[$k].'"'.$sel.'>'.$countriesNames[$k].' ('.$countriesCodes[$k].")</option>\n";
+}
 
-	$Ltemplate->assign_vars( array(	 	
-		'TAKEOFF_OPTION_LIST'=>$TAKEOFF_OPTION_LIST,
-		'COUNTRIES_OPTION_LIST'=>$COUNTRIES_OPTION_LIST,
-		'MODULE_REL_PATH'=>$moduleRelPath,
+for($k=0, $TAKEOFF_OPTION_LIST=''; $k<count($takeoffs); ++$k)
+{
+	$sel=($takeoffsID[$k]==$FILTER_takeoff1_select) ? ' selected' : '';
+	$TAKEOFF_OPTION_LIST.='<option value="'.$takeoffsID[$k].'"'.$sel.'">'.$takeoffs[$k]."</option>\n";
+}
+
+$Ltemplate->assign_vars(array(
+	'TAKEOFF_OPTION_LIST'	=>$TAKEOFF_OPTION_LIST,
+	'COUNTRIES_OPTION_LIST'	=>$COUNTRIES_OPTION_LIST,
+	'MODULE_REL_PATH'		=>$moduleRelPath,
 	));
 
-   	$Ltemplate->pparse('body');
+$Ltemplate->pparse('body');
+
 ?>
