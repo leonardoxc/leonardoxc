28c3f5ae88a09bf670037f9356c392f66d069ee7
manolis <manolis>
2006-10-08 22:20:40 +0000

    *** empty log message ***

--- MENU_menu.php
+++ MENU_menu.php
@@ -1,4 +1,5 @@
-<? 
+<?php
+
 /************************************************************************/
 /* Leonardo: Gliding XC Server					                        */
 /* ============================================                         */
@@ -11,18 +12,19 @@
 /* it under the terms of the GNU General Public License as published by */
 /* the Free Software Foundation; either version 2 of the License.       */
 /************************************************************************/
+
 ?>
-<link rel="stylesheet" type="text/css" href="<? echo $moduleRelPath ?>/menu/themes/office_xp/office_xp.css">
-<script type="text/javascript" src="<? echo $moduleRelPath ?>/menu/jsdomenu.js"></script>
-<script type="text/javascript" src="<? echo $moduleRelPath ?>/menu/jsdomenubar.js"></script>
+<link rel="stylesheet" type="text/css" href="<?=$moduleRelPath?>/menu/themes/office_xp/office_xp.css"/>
+<script type="text/javascript" src="<?=$moduleRelPath?>/menu/jsdomenu.js"></script>
+<script type="text/javascript" src="<?=$moduleRelPath?>/menu/jsdomenubar.js"></script>
 
 <script language="javascript">
-	<? require_once $moduleRelPath."/jsdomenu.inc.js.php"; ?>
-	window.onload = initjsDOMenu;
+	<?php require_once $moduleRelPath.'/jsdomenu.inc.js.php'; ?>
+	window.onload=initjsDOMenu;
 	// initjsDOMenu();
 </script>
 
-<div class="main_text" align=center>
-<a name="top_of_page"></a>
-<div id="staticMenuPos" align=left></div>
-<br>
+<div class="main_text" align="center">
+	<a name="top_of_page"></a>
+	<div id="staticMenuPos" align="left"></div>
+	<br/>
