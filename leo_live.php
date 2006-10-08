d9e2ea656f992a3f4603476114933f3c3c297447
manolis <manolis>
2006-10-08 15:40:45 +0000

    *** empty log message ***

--- leo_live.php
+++ leo_live.php
@@ -11,6 +11,7 @@
 /* the Free Software Foundation; either version 2 of the License.       */
 /************************************************************************/
 
+
  	require_once "EXT_config_pre.php";
 	require_once "config.php";
  	require_once "EXT_config.php";
