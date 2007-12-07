94731019c407dd0eb9928cf767a728e308f6a987
manolis <manolis>
2007-12-07 15:46:27 +0000

    *** empty log message ***

--- site/config_custom.php
+++ site/config_custom.php
@@ -322,7 +322,7 @@
 	$CONF['brands']['list']=array();
 	$CONF['brands']['filterList']=array(1,2,4);
 
-
+	//$CONF['servers']['list']=array( );
 	$CONF['servers']['list']=array( 0=>"Local Server", 1=>"paraglidingforum.com", 2=>"sky.gr",3=>"cyclone.com.br" );
 	
 ?>
