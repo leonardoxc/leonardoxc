30900be6c625ff6a6eb8cf75b00c931991c2576e
manolis <manolis>
2007-12-20 23:29:10 +0000

    *** empty log message ***

--- GUI_waypoint_edit.php
+++ GUI_waypoint_edit.php
@@ -43,42 +43,38 @@
 	$waypt=new waypoint($waypointIDedit);
 	$waypt->getFromDB();
 
+	require_once dirname(__FILE__).'/FN_editor.php';
+
     echo "<br>";
 
-    open_inner_table("Edit waypoint",650,"icon_pin.png");
+    open_inner_table("Edit waypoint",760,"icon_pin.png");
  
   echo "<tr><td>";	
 ?> 
 
       <form name="form1" method="post" action="">
 
-        <table width="600" border="0" align="center" cellpadding="2" class=main_text>
+        <table width="725" border="0" align="center" cellpadding="2" class=main_text>
           <tr>
-            <td width="201" bgcolor="#CFE2CF"><div align="right"><font color="#003366">Name</font></div></td>
-            <td bgcolor="#E3E7F2"><font color="#003366">
-              <input name="wname" type="text" id="wname" size="40" value="<? echo $waypt->name ?>" >
+            <td width="104" bgcolor="#CFE2CF"><div align="right"><font color="#003366">Name</font></div></td>
+            <td width="212" bgcolor="#E3E7F2"><font color="#003366">
+              <input name="wname" type="text" id="wname" size="30" value="<? echo $waypt->name ?>" >
               <input name="waypointIDedit " type="hidden" id="waypointIDedit " value="<? echo $waypointIDedit ?>" >
               <input name="type" type="hidden" id="type" value="<? echo $waypt->type ?>" >
             </font></td>
-          </tr>
-          <tr>
-            <td bgcolor="#CFE2CF"><div align="right"><font color="#003366">International
-            Name</font></div></td>
-            <td bgcolor="#E3E7F2"><font color="#003366">
-              <input name="intName" type="text" id="intName" size="40" value="<? echo $waypt->intName ?>" >
+            <td width="143" bgcolor="#CFE2CF"><div align="right"><font color="#003366">Name in English <img src="<?=$moduleRelPath?>/language/flag-english.png" width="18" height="12" /></font></div></td>
+            <td width="240" bgcolor="#E3E7F2"><font color="#003366">
+              <input name="intName" type="text" id="intName" size="30" value="<? echo $waypt->intName ?>" />
             </font></td>
           </tr>
           <tr>
             <td bgcolor="#CFE2CF"><div align="right"><font color="#003366">Region</font></div></td>
             <td bgcolor="#E3E7F2"><font color="#003366">
-              <input name="location" type="text" id="Location" size="40" value="<? echo $waypt->location ?>" >
+              <input name="location" type="text" id="Location" size="30" value="<? echo $waypt->location ?>" >
             </font></td>
-          </tr>
-          <tr>
-            <td bgcolor="#CFE2CF"><div align="right"><font color="#003366">International
-            Region</font></div></td>
+            <td bgcolor="#CFE2CF"><div align="right"><font color="#003366">Region in English <img src="<?=$moduleRelPath?>/language/flag-english.png" width="18" height="12" /></font></div></td>
             <td bgcolor="#E3E7F2"><font color="#003366">
-              <input name="intLocation" type="text" id="intLocation" size="40" value="<? echo $waypt->intLocation ?>" >
+              <input name="intLocation" type="text" id="intLocation" size="30" value="<? echo $waypt->intLocation ?>" />
             </font></td>
           </tr>
           <tr>
@@ -96,38 +92,33 @@
 				?>
 		    </select>
             </font></td>
-          </tr>
-          <tr>
-            <td width="201" bgcolor="#CFE2CF">
-            <div align="right"><font color="#003366">lat</font></div></td>
-            <td width="385" bgcolor="#E3E7F2"><font color="#003366">
-              <input name="lat" type="text" id="lat"  value="<? echo $waypt->lat ?>" >
-            </font></td>
-          </tr>
-          <tr>
-            <td bgcolor="#CFE2CF"><div align="right"><font color="#003366">lon</font></div></td>
-            <td bgcolor="#E3E7F2"><font color="#003366">
-              <input name="lon" type="text" id="lon"  value="<? echo $waypt->lon ?>" >
+            <td colspan="2" bgcolor="#CFE2CF"><font color="#003366">lat
+              <input name="lat" type="text" id="lat"  size="10" value="<? echo $waypt->lat ?>" />
+            lon
+            <input name="lon" type="text" id="lon" size="10" value="<? echo $waypt->lon ?>" />
             </font></td>
           </tr>
           <tr>
             <td bgcolor="#CFE2CF"><div align="right"><font color="#003366">Relevant URL</font></div></td>
-            <td bgcolor="#E3E7F2"><font color="#003366">
+            <td colspan="3" bgcolor="#E3E7F2"><font color="#003366">
               <input name="link" type="text" id="link"  value="<? echo $waypt->link ?>" size="60" >
+              <input type="submit" name="Submit2" value="<? echo "APPLY CHANGES" ?>" />
             </font></td>
           </tr>
           <tr>
-            <td valign="top" bgcolor="#CFE2CF"><div align="right"><font color="#003366">Description</font></div></td>
-            <td bgcolor="#E3E7F2"><font color="#003366">
-              <textarea name="description" cols="60" rows="5" id="description"><? echo $waypt->description ?></textarea>
-            </font></td>
+            <td colspan="4" bgcolor="#E7E6CB"><div align="center"><strong><font color="#003366">Description</font></strong></div></td>
           </tr>
           <tr>
-            <td bgcolor="#CFE2CF"><div align="right"></div></td>
-            <td bgcolor="#E3E7F2">
-				<input type="submit" name="Submit" value="<? echo "APPLY CHANGES" ?>">
-				<input type="hidden" name="editWaypoint" value="1">		
-  	     </td>
+            <td colspan="4" valign="top" bgcolor="#CFE2CF">
+			  <? createTextArea('description',$waypt->description ,'Leonardo',710,600);
+			?>            </td>
+          </tr>
+          <tr>
+            <td colspan="4"><div align="right"></div>
+				<div align="center">
+				  <input type="submit" name="Submit" value="<? echo "APPLY CHANGES" ?>">
+				  <input type="hidden" name="editWaypoint" value="1">  	     
+		        </div></td>
           </tr>
         </table>
 </form>
---
+++ js/fckeditor/config_leonardo.js
@@ -0,0 +1,21 @@
+﻿FCKConfig.ToolbarSets["Leonardo"] = [
+['Source','DocProps','-','Save','NewPage','Preview','-','Templates'],
+['Cut','Copy','Paste','PasteText','PasteWord','-','Print','SpellCheck'],
+['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+['About'] ,
+['Bold','Italic','Underline','TextColor','BGColor','-','Subscript','Superscript'],
+['OrderedList','UnorderedList','-','Outdent','Indent'],
+['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+['Link','Unlink','Anchor'],
+['Image','Flash','Table','Rule','SpecialChar','Smiley'],
+'/',
+['Style','FontFormat','FontName','FontSize']
+] ;
+
+FCKConfig.ToolbarSets["ads"] = [
+['Save','Cut','Copy','Paste'],
+'/',
+['Image','Flash','Link','Unlink'],
+'/',
+['Bold','TextColor','Rule']
+] ;
