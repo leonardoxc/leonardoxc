19138f5109cc384b329fca219482ccf5a970a294
manolis <manolis>
2008-01-22 00:43:26 +0000

    *** empty log message ***

--- syncWXC.php
+++ syncWXC.php
@@ -26,7 +26,7 @@ require_once "FN_pilot.php";
 require_once "CL_server.php";
 require_once "lib/json/CL_json.php";
 
-$ServerId="Le0nard0"; // should get entry from from DB 
+$ServerId=$CONF_server_id;//"Le0nard0"; // should get entry from from DB 
 //echo "from=".(time()-3600*24*10)."&to=".time();
 //exit;
 
@@ -52,11 +52,18 @@ if (isset($_GET['to'])) {
     $to=time();
  }
 
+$zerocivl=0;
+if (isset($_GET['zerocivl'])) { 
+   $zerocivl=makeSane($_GET['zerocivl'],1);
+ }
+
+
+
 $clientID	= makeSane($_GET['clientID'],1);	
 $clientPass	= makeSane($_GET['clientPass'],0);
 
-if ($count) $limit="LIMIT $count";
- else $limit="";
+//if ($count) $limit="LIMIT $count";
+// else $limit="";
 
 $where_clause=" AND ItemType=1 ";
 
@@ -66,7 +73,23 @@ if ( !Server::checkServerPass($clientID,$clientPass)) {
   die();
  }
 
-$query="SELECT * FROM $logTable  WHERE  actionTime>$from  AND actionTime<=$to AND  result=1 $where_clause ORDER BY actionTime  $limit";
+
+
+
+$query="select max(n) from (select count(actionTime) as n from $logTable where   actionTime>=$from  AND actionTime<$to AND result=1 $where_clause group by actionTime) as x";
+
+$res= $db->sql_query($query);
+if($res <= 0){
+  echo("<H3> Error in query $query</H3>\n");
+  exit();
+ }
+$row = mysql_fetch_assoc($res);
+if($row["max(n)"]>$count) $count=$row["max(n)"]+0;
+
+
+ 
+ 
+$query="SELECT * FROM $logTable  WHERE  actionTime>=$from  AND actionTime<$to AND result=1 $where_clause ORDER BY transactionID  $limit";
 
 $res= $db->sql_query($query);
 if($res <= 0){
@@ -78,18 +101,33 @@ $item_num=0;
 $item_ok=0;
 while ($row = mysql_fetch_assoc($res)) { 
   
+  // get some data from log database
   $actionTime=$row['actionTime'];
+  $ItemID=$row['ItemID'];
+  $action=$row['ActionID']; // i.e. 1-inset, 2-update, 4-delete
+ 
+
 //  $Action=json::decode( utf8_encode( $row['ActionXML']) );
+
+
+
   $Action=json::decode( $row['ActionXML'] );
-	//print_r( $Action);
 
 
   if($Action){
     $flight = read_leonardo_json_flight($Action['flight']);
 
     // check if civl is given and >0
-    if(makeSane($flight['CivlId'],1)>0 || 1) {
+    if(makeSane($flight['CivlId'],1)>0 || $zerocivl) {
       $flight['LastTimestamp'] = $actionTime;
+      $flight['NacFlightId'] = $ItemID;
+
+      if($action=="4"){
+	$flight['NacStatus'] = "d";
+      }
+
+
+
       foreach($flight as $key => $value){
 			$FlighsXML[$item_ok].= '      '."<".$key.">".$value."</".$key.">"."\n";
       }
@@ -114,7 +152,7 @@ header ('Content-Type: text/xml;');
 
 echo '<?xml version="1.0" encoding="utf-8" ?>'."\n";
 echo '<!DOCTYPE FlightSync PUBLIC "-//XCC Nonprofit Organisation//DTD FlightSync Library//EN" "http://www.xccomp.org/spec/xcc-flightsync.dtd">'."\n";
-echo '<FlightSync version="0.6">'."\n";
+echo '<FlightSync version="0.7">'."\n";
 echo '<SyncInfo>'."\n";
 echo '    <ServerId>'.$ServerId.'</ServerId>'."\n";
 echo '    <NumberOfItems>'.$item_ok.'</NumberOfItems>'."\n";
@@ -145,44 +183,83 @@ function read_leonardo_json_flight($flight){
   $IgcMd5=$flight['validation']['hash'];
   $LastTimestamp="0";// actionTimeUTC" not in "flight"
   $FirstTimestamp=strtotime($flight['dateAdded']);
-  $NacStatus="";
+
+  if($flight['validation']['validated']=="1")
+    $NacStatus="o";
+  else
+    $NacStatus="";
+
   $NacPilotId=$flight['pilot']['userID'];
-  $NacFlightId=$flight['id'];
+  $NacFlightId="0";// $flight['id']; in "fligths"  item is the original, not master ID
+
+
+
+  $PilotFirstName =  html_entity_decode($flight['pilot']['pilotFirstName']);
+  $PilotLastName =  html_entity_decode($flight['pilot']['pilotLastName']);
+  $PilotNation   =  strtoupper($flight['pilot']['pilotCountry']);
+  $PilotGender   =  $flight['pilot']['pilotSex'];
 
 
   switch ($flight['info']['gliderCat']){
   case "0":
-    $GliderCat="0000";
+    $GliderCat="0";
     break;
-  case 1:
-    $GliderCat="3000";    
+  case "1":
+    $GliderCat="3";    
     break;
-  case 2:
-    $GliderCat="1000";    
+  case "2":
+    $GliderCat="1";    
     break;
-  case 4:
-  $GliderCat="5000";    
-  break;
+  case "4":
+    $GliderCat="5";    
+    break;
+  default:
+    $GliderCat="9";    // non free glider
+  }
 
-}
+  switch ($flight['info']['cat']){
+
+  case "1":
+    $GliderCat.="201";    
+    break;
+  case "2":
+    $GliderCat.="901";    
+    break;
+  case "3":
+    $GliderCat.="902";    
+    break;
+
+  default :
+    $GliderCat.="901";    
+    break;
+    
+  }
+
+
+
+
   $Glider=html_entity_decode($flight['info']['glider'],ENT_NOQUOTES,"UTF-8");
     
-  $dte=$flight->time->date;
+  $dte=$flight['time']['date'];
   $dte=explode("-",$dte);
-  
+ 
+
   $TakeoffTime =  gmmktime(0, 0, 0, intval($dte[1]), intval($dte[2]), intval($dte[0]))+$flight['bounds']['firstTM'];
   $LandingTime =  gmmktime(0, 0, 0, intval($dte[1]), intval($dte[2]), intval($dte[0]))+$flight['bounds']['lastTM'];
 
   $TakeoffName=html_entity_decode($flight['location']['takeoffName'],ENT_NOQUOTES,"UTF-8");
   $TakeoffCountry=$flight['location']['takeoffCountry'];
 
-  $CommentInternal=html_entity_decode($flight['info']['comments'],ENT_NOQUOTES,"UTF-8");
+  $CommentInternal=htmlspecialchars(html_entity_decode($flight['info']['comments'],ENT_NOQUOTES,"UTF-8") );
   
-  //  $FlightUrl=html_entity_decode($flight->linkDisplay,ENT_NOQUOTES,"UTF-8");
   $FlightUrl=$flight['linkDisplay'];
 
   return array( 
 	       'CivlId'=>$CivlId,
+	       'PilotFirstName'=>$PilotFirstName, 
+	       'PilotLastName'=>$PilotLastName,  
+	       'PilotNation'=>$PilotNation,    
+	       'PilotGender'=>$PilotGender,     
 	       'CivlIdApprovedBy'=>$CivlIdApprovedBy,
 	       'IgcUrl'=>$IgcUrl,
 	       'IgcMd5'=>$IgcMd5,
