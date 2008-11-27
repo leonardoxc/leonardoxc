2399ed195ac4b17f40ccde62c369bd93c9ec9c33
manolis <manolis>
2008-11-27 11:10:38 +0000

    *** empty log message ***

--- site/config_admin_email.php
+++
@@ -1,1 +0,0 @@
-<? $CONF_admin_email="andread@thenet.gr"; $CONF_SitePassword="29nnfsd02hnvdoqg12we2"; ?>
--- site/config_admin_users.php
+++
@@ -1,8 +0,0 @@
-<?
-$admin_users=array(2,96,76,95,228,672,62);  
-
-// 672->manolis at DHV db
-// 62-> admin at joomla.home
-	// move some flights to Manolis 
-	//update `leonardo_flights` set `userID`=672  where `userID`=626
-?>
