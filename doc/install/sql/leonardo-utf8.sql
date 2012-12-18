25deaf97911918afcbae96b6023b50962e3dc244
Fernando García Samblas <fernando.garcia@the-cocktail.com>
2012-12-18 19:37:55 +0100

    fixed field types

--- doc/install/sql/leonardo-utf8.sql
+++ doc/install/sql/leonardo-utf8.sql
@@ -419,8 +419,8 @@ CREATE TABLE `leonardo_pilots` (
   `BirthdateHideMask` varchar(10) NOT NULL default 'xx.xx.xxxx',
   `PilotPhoto` varchar(30) NOT NULL default '',
   `FirstOlcYear` int(10) NOT NULL default '0',
-  `FirstNameEn` int(40) NOT NULL default '',
-  `LastNameEn` int(50) NOT NULL default '',
+  `FirstNameEn` varchar(40) NOT NULL default '',
+  `LastNameEn` varchar(50) NOT NULL default '',
   PRIMARY KEY  (`pilotID`,`serverID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
