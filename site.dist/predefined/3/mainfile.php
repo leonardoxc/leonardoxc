5f9330f481eb4a7cadb00dac0448a7a2a894eb90
evandroamaro <evandroamaro@sapo.pt>
2014-02-18 02:17:35 +0000

    Deprecathed Var HTTP_SERVER_VARS and undefined $HTTP_USER_AGENT . Now should use $_SERVER.

--- site.dist/predefined/3/mainfile.php
+++ site.dist/predefined/3/mainfile.php
@@ -5,13 +5,13 @@ $phpver = phpversion();
 
 if (1) {
 
-if ($phpver >= '4.0.4pl1' && strstr($HTTP_USER_AGENT,'compatible')) {
+if ($phpver >= '4.0.4pl1' && strstr($_SERVER['HTTP_USER_AGENT'],'compatible')) {
     if (extension_loaded('zlib')) {
         ob_end_clean();
         ob_start('ob_gzhandler');
     }
 } else if ($phpver > '4.0') {
-    if (strstr($HTTP_SERVER_VARS['HTTP_ACCEPT_ENCODING'], 'gzip')) {
+    if (strstr($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) {
         if (extension_loaded('zlib')) {
             $do_gzip_compress = TRUE;
             ob_start();
